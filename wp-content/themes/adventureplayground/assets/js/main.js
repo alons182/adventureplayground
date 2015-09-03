cf8fac40ce28fd9cefe71f1424702610d6887e07
Alonso <alons182@hotmail.com>
2015-09-03 13:15:58 -0600

    github

--- wp-content/themes/adventureplayground/assets/js/main.js
+++ wp-content/themes/adventureplayground/assets/js/main.js
@@ -155,7 +155,7 @@
 
                 $.ajax({
                       type: 'GET',
-                      url: '/api/get_post/?post_id='+ post_id +'&post_type=projects',
+                      url: '/adventureplayground/api/get_post/?post_id='+ post_id +'&post_type=projects',
                       
                       success: function(data){
                          console.log(data)
