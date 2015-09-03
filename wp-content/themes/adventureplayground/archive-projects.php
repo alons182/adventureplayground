fe7ff9fff3f7c9008a6aeb19b4e59771b2e994c4
Alonso <alons182@hotmail.com>
2015-09-03 13:22:18 -0600

    fix banner

--- wp-content/themes/adventureplayground/archive-projects.php
+++ wp-content/themes/adventureplayground/archive-projects.php
@@ -1,47 +1,5 @@
 <?php get_header(); ?>
 
-<section class="banner">
-        <div class="banner__video">
-            <video autoplay="autoplay" loop="loop" poster="http://lorempixel.com/960/360/nature/1/" id="bgvid">
-                        <source src="<?php echo get_template_directory_uri();  ?>/img/bg.mp4" type="video/mp4" />
-                        <source src="<?php echo get_template_directory_uri();  ?>/img/bg.ogv" type="video/ogg" />HTML5 vídeo no es soportado por este navegador</video>
-        </div>
-        <div class="banner__menu inner">
-            <article class="banner__menu__item">
-                <a href="#" class="banner__menu__item__link">
-                    <img src="http://lorempixel.com/257/164/sports/1/" alt="1" />
-                    <span class="banner__menu__item__readmore">Read more +</span>
-                </a>
-                <h2 class="banner__menu__item__title">Lorem ipsum dolor</h2>
-                 
-            </article>
-            <article class="banner__menu__item">
-                <a href="#" class="banner__menu__item__link">
-                    <img src="http://lorempixel.com/257/164/sports/2/" alt="2" />
-                    <span class="banner__menu__item__readmore">Read more +</span>
-                </a>
-                <h2 class="banner__menu__item__title">Lorem ipsum dolor</h2>
-                
-            </article>
-            <article class="banner__menu__item">
-                <a href="#" class="banner__menu__item__link">
-                    <img src="http://lorempixel.com/257/164/sports/3/" alt="3" />
-                    <span class="banner__menu__item__readmore">Read more +</span>
-                </a>
-                <h2 class="banner__menu__item__title">Lorem ipsum dolor</h2>
-                
-            </article>
-            <article class="banner__menu__item">
-                <a href="#" class="banner__menu__item__link">
-                    <img src="http://lorempixel.com/257/164/sports/4/" alt="4" />
-                    <span class="banner__menu__item__readmore">Read more +</span>
-                </a>
-                <h2 class="banner__menu__item__title">Lorem ipsum dolor</h2>
-                 
-            </article>
-            
-        </div>
-    </section>
 
     <main class="main">
         <section class="content">
--- wp-content/themes/adventureplayground/page-home.php
+++ wp-content/themes/adventureplayground/page-home.php
@@ -5,26 +5,6 @@
  ?>
 <?php get_header(); ?>
 
-<section class="banner">
-            <div class="banner__video">
-                <video autoplay="autoplay" loop="loop" poster="http://lorempixel.com/960/360/nature/1/" id="bgvid">
-                            <source src="<?php echo get_template_directory_uri();  ?>/img/bg.mp4" type="video/mp4" />
-                            <source src="<?php echo get_template_directory_uri();  ?>/img/bg.ogv" type="video/ogg" />HTML5 vídeo no es soportado por este navegador</video>
-            </div>
-            <div class="banner__menu inner">
-              
-              <?php query_posts( 'post_type=page&page_id=286' ); ?>
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-                        <?php the_content(); ?>
-        
-                    <?php endwhile; ?>
-                    <!-- post navigation -->
-                  
-                <?php endif; ?>
-                <?php wp_reset_query(); ?>
-            </div>
-        </section>
 
         <main class="main">
             <section class="map">
